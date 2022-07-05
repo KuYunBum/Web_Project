efb46f023cbe76cac4270c70a063b6c1ce1dc550
Colrow <Colrow@DESKTOP-JAJ9UUL>
2022-07-05 17:00:48 +0900

    4 commit

---
+++ project/src/main/java/com/spring/dto/UserRecord.java
@@ -0,0 +1,49 @@
+package com.spring.dto;
+
+import java.util.Date;
+
+
+public class UserRecord {
+	private int userNum;
+	private Date startTime;
+	private Date endTime;
+	private Date totalTime;
+	private Date useDate;
+	public int getUserNum() {
+		return userNum;
+	}
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	public Date getTotalTime() {
+		return totalTime;
+	}
+	public void setTotalTime(Date totalTime) {
+		this.totalTime = totalTime;
+	}
+	public Date getUseDate() {
+		return useDate;
+	}
+	public void setUseDate(Date useDate) {
+		this.useDate = useDate;
+	}
+	@Override
+	public String toString() {
+		return "UserRecord [userNum=" + userNum + ", startTime=" + startTime + ", endTime=" + endTime + ", totalTime="
+				+ totalTime + ", useDate=" + useDate + "]";
+	}
+	
+	
+}
